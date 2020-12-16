5ab9ea29fb5ef4fdb80d57cdb9f3449107e1d718
dranel.AI <lenarddumalagan25@gmail.com>
2020-12-16 13:59:47 +0800

    add ejs

---
+++ server.js
@@ -0,0 +1,19 @@
+// imports
+const express = require('express')
+const app = express()
+
+// static file
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/img', express.static(__dirname + 'public/img'))
+app.use('/js', express.static(__dirname + 'public/js'))
+
+// views set
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('', (req,res) => {
+    res.render('index')
+})
+
+app.listen(3000)
